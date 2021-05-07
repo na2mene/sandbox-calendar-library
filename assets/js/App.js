19be3e240f8ddd15b4c8caca17afe0e463380b46
taku.tsunose <taku.tsunose@medley.jp>
2021-05-07 11:53:14 +0900

    create mock

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,5 +1,7 @@
 const App = {
   calendar: null,
+  WeekChars: [ '日', '月', '火', '水', '木', '金', '土' ],
+  displayDate: null,
 
   initialize: function() {
     const self = this;
@@ -13,8 +15,21 @@ const App = {
       disableMobile: true,
       inline: true,
       locale: 'ja',
+      altInput: true,
+      altFormat: "",
+      dateFormat: 'Y-m-d',
+      onChange: self.onChangeCalendar,
     };
     self.calendar = $('.selector').flatpickr(config);
+  },
+
+  onChangeCalendar: function(selectedDates, dateStr, instance) {
+    const targetDate = new Date(dateStr);
+    App.displayDate = `${targetDate.getFullYear()}年${targetDate.getMonth() + 1}月${targetDate.getDate()}日 (${App.WeekChars[targetDate.getDay()]})`;
+    console.log(App.displayDate);
+
+    // DOM動的生成
+    $('#labelTime').text(App.displayDate);
   },
 
   eventLoader: function() {
